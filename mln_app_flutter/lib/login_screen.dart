835b79857d3438b62f096d6afed8fb3180ccbc87
Khanh Nguyen Tan <khanhnt748@gmail.com>
2019-09-09 17:28:31 +0700

    \!Integrating Google Firebase for Android (Todo: also for iOS)

--- mln_app_flutter/lib/login_screen.dart
+++ mln_app_flutter/lib/login_screen.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'package:mln_app_flutter/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -41,7 +44,7 @@ class _LoginScreenState extends State<LoginScreen> {
           child:Column(
             children: <Widget>[
               Expanded(
-                flex: 2,
+                flex: 5,
                 child: Center(
                   child: Image.asset("assets/milano_logo.png"),
                 ),
@@ -60,59 +63,78 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget _buildLoginForm () {
     return Expanded(
-      flex: 3,
+      flex: 6,
       child: Container(
         decoration: BoxDecoration(
           color: Colors.white70,
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text(
-                'Đăng nhập',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: EdgeInsets.only(left: 75, right: 75),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  'Đăng nhập',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 75, right: 75),
-                child: Container(
-                  color: Colors.black45,
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black45,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
                   child: TextFormField(
                     style: TextStyle(color: Colors.white),
                     controller: textController,
                     keyboardType: TextInputType.text,
+                    textAlign: TextAlign.center,
                     decoration: InputDecoration(
-                      hintText: '\tNhập số điện thoại',
-                      hintStyle: TextStyle(color: Colors.white),
-                      contentPadding: EdgeInsets.all(8)
+                      hintText: 'Nhập số điện thoại',
+                      hintStyle: TextStyle(color: Colors.black),
+                      border: InputBorder.none,
+                      suffixIcon: Icon(Icons.arrow_forward_ios)
                     ),
                   ),
                 ),
-              ),
-              Text('Hello'),
-              Padding(
-                padding: EdgeInsets.only(left: 75, right: 75),
-                child: Container(
-                  color: Colors.black45,
-                  child: TextFormField(
-                    style: TextStyle(color: Colors.white),
-                    controller: textController,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                        hintText: '\tNhập số điện thoại',
-                        hintStyle: TextStyle(color: Colors.white),
-                        contentPadding: EdgeInsets.all(8)
+                Text('Hoặc đăng nhập qua mạng xã hội'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: FlatButton.icon(
+                          onPressed: () => Utility.handleSignIn().then((FirebaseUser user) => print(user)).catchError((e) => print(e)),
+//                          onPressed: null,
+                          color: Colors.blue,
+                          icon: Icon(Icons.error),
+                          label: Text('Facebook'),
+                      ),
                     ),
-                  ),
+                    Container(
+                      height: 45,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: FlatButton.icon(
+                          onPressed: () => Utility.handleSignIn().then((FirebaseUser user) => print(user)).catchError((e) => print(e)),
+//                          onPressed: null,
+                          color: Colors.red,
+                          icon: Icon(Icons.error),
+                          label: Text('Google')
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Text('Hello'),
-              Text('Hello'),
-            ],
-          )
+                Text('Hello'),
+              ],
+            ),
+        )
       ),
     );
   }
--- mln_app_flutter/lib/main.dart
+++ mln_app_flutter/lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:mln_app_flutter/route_controller.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+void main() {
+  runApp(MainApp());
+}
 
-void main() => runApp(MainApp());
+class Utility {
+  static final GoogleSignIn _googleSignIn = GoogleSignIn();
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  static Future<FirebaseUser> handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+    FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+    print("signed in " + user.displayName);
+    return user;
+  }
+
+}
 
 class MainApp extends StatelessWidget {
   @override
